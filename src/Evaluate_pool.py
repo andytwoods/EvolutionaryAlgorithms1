682696d8385353735c31b9529a1a368aae83532e
andytwoods <andy.woods@xperiment.mobi>
2016-03-08 17:31:43 +0000

    added sanity 'parameters' feature

--- src/Evaluate_pool.py
+++ src/Evaluate_pool.py
@@ -2,36 +2,52 @@ import threading
 
 
 class Evaluate_pool(object):
-    def __init__(self, callback, minimum_finished, num_evals = -1):
+
+
+
+    def __init__(self, callback, params):
+        self._add_defaults(params)
+        self.params = params
         self.callback = callback
         self.pool = list()
-        self.minimum_finished = minimum_finished
-        self.num_evals = num_evals
         self.potentially_tested_pool = list()
         self.tested_pool = list()
         self.id_counter = 0
 
+    @staticmethod
+    def _add_defaults(params):
+
+        def check_add(what, default):
+            if params.get(what) is None:
+                params[what] = default
+
+        check_add('minimum_finished', 5)
+        check_add('num_evals', 5)
+        check_add('timeout',60)
+
     def add(self, item):
-        self.pool.append(Individual(item, self.id_counter, self._callback_individual, self.num_evals))
+        self.pool.append(Individual(item, self.id_counter, self._callback_individual, self.params))
         self.id_counter += 1
 
     def add_many(self, items):
         for item in items:
             self.add(item)
 
-    def _callback_individual(self, individual, message):
-        if message == "data collected":
-            self.potentially_tested_pool.remove(individual)
-            self.tested_pool.append(individual)
-            if len(self.tested_pool) >= self.minimum_finished:
-                test_these = list()
-                while len(self.tested_pool) > 0:
-                    test_these.append(self.tested_pool.pop(0))
-                self.callback(test_these)
+    def _callback_individual(self, individual):
+
+        self.potentially_tested_pool.remove(individual)
+        self.tested_pool.append(individual)
+
+        if len(self.tested_pool) >= self.params.get('minimum_finished'):
+            test_these = list()
+            while len(self.tested_pool) > 0:
+                test_these.append(self.tested_pool.pop(0))
+            self.callback(test_these)
 
     def get_item(self, rater):
 
-        if len(self.pool)==0: return None
+        if len(self.pool) is 0:
+            return None
 
         item = self.pool[0]
         info = item.rating_request(rater)
@@ -42,10 +58,10 @@ class Evaluate_pool(object):
 
         return info
 
-    def return_item(self, rating, rating_num, id):
+    def return_item(self, rating, rating_num, rating_id):
         for given_pool in [self.pool, self.potentially_tested_pool]:
             for item in given_pool:
-                if item.id == id:
+                if item.id == rating_id:
                     item.rated(rating, rating_num)
                     return
 
@@ -53,9 +69,8 @@ class Evaluate_pool(object):
 
 
 class Individual(object):
-    default_num_evaluations = 5
 
-    def __init__(self, item, individual_id, callback, number_evaluations=-1):
+    def __init__(self, item, individual_id, callback, params):
         self.item = item
         self.id = individual_id
         self.callback = callback
@@ -65,17 +80,14 @@ class Individual(object):
         self.potentially_finished = False
         self.data = None
         self.finished = False
-        if number_evaluations is -1:
-            self.default_num_evaluations = self.default_num_evaluations
-        else:
-            self.default_num_evaluations = number_evaluations
+        self.params = params
 
-        self.new_rating(self.default_num_evaluations)
+        self.new_rating(self.params.get('num_evals'))
 
     def rating_request(self, rater):
 
         rating = self.to_be_rated.pop(0)
-        rating.start(rater, self.callback_rating, False)
+        rating.start(rater, self.fail_callback_rating)
         self.in_limbo.insert(0, rating)
 
         if len(self.to_be_rated) == 0:
@@ -86,23 +98,25 @@ class Individual(object):
     def new_rating(self, how_many):
         for i in range(0, how_many):
             rating_num = len(self.to_be_rated)
-            self.to_be_rated.append(Rating(rating_num))
+            self.to_be_rated.append(Rating(rating_num, self.params))
 
-    def callback_rating(self, rating, action):
-        if action == 'late':
-            self.new_rating(1)
+    def fail_callback_rating(self):
+        self.new_rating(1)
 
     def rated(self, score, rating_num):
 
         for rating in self.in_limbo:
+
             if rating.rating_num == rating_num:
                 rating.rated(score)
                 self.in_limbo.remove(rating)
-                self.has_been_rated.insert(0, rating)
-                if len(self.has_been_rated) == self.default_num_evaluations:
+                self.has_been_rated.append(rating)
+
+                if len(self.has_been_rated) >= self.params.get('num_evals'):
                     self.finished = True
                     self.data = self.compile_data()
-                    self.callback(self, 'data collected')
+                    self.callback(self) #tell parent that data is collected
+
                 return
 
         raise ValueError('When returning a rating, the rating number was not found. Devel error.')
@@ -110,31 +124,28 @@ class Individual(object):
     def compile_data(self):
         dat = []
         for rating in self.has_been_rated:
-            dat.insert(0, rating.rating)
+            dat.append(rating.rating)
         return dat
 
 class Rating(object):
-    timeout = 60
 
-    def __init__(self, rating_num):
+
+    def __init__(self, rating_num, params):
+        self.rating_num = rating_num
+        self.params = params
         self.rating = None
         self.rater = None
         self.timer = None
-        self.callback = None
-        self.rating_num = rating_num
+        self.fail_callback = None
 
-    def start(self, rater, callback, force_timeout=False):
+    def start(self, rater, fail_callback):
         self.rater = rater
 
-        self.callback = callback
-        self.timer = threading.Timer(Rating.timeout, self.do_callback)
-
-        if force_timeout is True:
-            self.timer.cancel()
-            self.do_callback()
+        self.fail_callback = fail_callback
+        self.timer = threading.Timer(self.params.get('time_out'), self.do_callback)
 
     def do_callback(self):
-        self.callback(self, 'late')
+        self.fail_callback()
 
     def rated(self, rating):
         self.stop_timeout()
--- tests/test_evaluate_pool.py
+++ tests/test_evaluate_pool.py
@@ -5,18 +5,19 @@ from random import random
 class TestRating(TestCase):
 
     def test_rating(self):
-        r = Rating(1)
-        self.assertEquals(r.rating_num, 1)
+        params = {'time_out': 1}
+        r = Rating(1, params)
+        self.assertEquals(r.rating_num, 1 )
 
-        r.start('rater', self.callback, 0)
+        r.start('rater', self.callback)
 
-    def callback(self, rating, message):
+    def callback(self):
         #on account of python async testing being too painful
         self.assertTrue(True)
 
     def test_rating_givenResponseWithinTime(self):
-
-        r = Rating(2)
+        params = {'time_out': 1}
+        r = Rating(2, params)
 
         r.start('rater2', self.callback)
         self.assertTrue(r.timer is not None)
@@ -28,19 +29,20 @@ class TestRating(TestCase):
         self.assertTrue(r.rating == 'banana')
 
 
-
 class TestIndividual(TestCase):
 
     def test_1(self):
 
         num_evals = 2
+        default_num_evaluations = 5
 
-        ind = Individual('my_item', 'my_id', self.callback, num_evals)
+        params = {'num_evals': num_evals, 'num_evals': 5}
+        ind = Individual('my_item', 'my_id', self.callback, params)
 
-        self.assertEquals(len(ind.to_be_rated), num_evals)
+        self.assertEquals(len(ind.to_be_rated), default_num_evaluations)
         request = ind.rating_request('rater')
 
-        self.assertTrue(len(ind.to_be_rated) == 1)
+        self.assertTrue(len(ind.to_be_rated) == 4)
         self.assertTrue(request.get('id') == 'my_id')
         self.assertTrue(request.get('rating_num') == 0)
         self.assertTrue(request.get('data') is not None)
@@ -55,24 +57,40 @@ class TestIndividual(TestCase):
 
         self.assertTrue(len(ind.has_been_rated) == 2)
 
-    def callback(self,_ind, message):
+    def test_failed_assessment(self):
+        params = {'num_evals': 2, 'num_evals': 5}
+        ind = Individual('my_item', 'my_id', self.callback, params)
+
+        self.assertEquals(len(ind.to_be_rated),5)
+        rating_request = ind.rating_request('rater')
+
+        failed_rating = ind.in_limbo[0]
+        self.assertEquals(len(ind.to_be_rated), 4)
+        failed_rating.do_callback()
+        self.assertEquals(len(ind.to_be_rated), 5)
+
+    def callback(self):
         self.assertTrue(True)
+
 
 
 class TestEvaluatePool(TestCase):
 
     def test_2(self):
-        feedback_after_x_complete_item_ratings = 3
 
         callback_self = self
 
-        def callback(items):
-            callback_self.assertEquals(len(items), 3)
+        def callback(_items):
+
+            callback_self.assertEquals(len(_items), 3)
             callback_self.assertEquals(len(eval.pool), 0)
+
             #there is 1 item left over in potentially_tested_pool as this function is not async - code still running
             callback_self.assertEquals(len(eval.potentially_tested_pool), 1)
 
-        eval = Evaluate_pool(callback, feedback_after_x_complete_item_ratings)
+
+        params = {'minimum_finished': 3, "num_evals":5}
+        eval = Evaluate_pool(callback, params)
 
         items_to_evaluate = 4
         items = list()
@@ -81,10 +99,12 @@ class TestEvaluatePool(TestCase):
             items.append(item)
 
         eval.add_many(items)
-        self.assertEquals(len(eval.pool), 4)
+
+        self.assertEquals(len(eval.pool), items_to_evaluate)
 
         counter = 0
         checkedout = eval.get_item('rater' + str(counter))
+
         self.assertEquals(checkedout.get('id'), 0)
         self.assertEquals(checkedout.get('rating_num'), 0)
 
@@ -96,7 +116,8 @@ class TestEvaluatePool(TestCase):
             checkedouts.append(checkedout)
             checkedout = eval.get_item('rater'+str(counter))
 
-        self.assertEquals(len(checkedouts), items_to_evaluate * Individual.default_num_evaluations)
+        # is 20
+        self.assertEquals(len(checkedouts), items_to_evaluate * params.get('num_evals'))
 
         self.assertEquals(len(eval.tested_pool),0)
 
@@ -108,21 +129,24 @@ class TestEvaluatePool(TestCase):
             dict[checkedout.get('id')] += 1
 
         for key in dict.keys():
-            self.assertEquals(dict.get(key), Individual.default_num_evaluations)
+            self.assertEquals(dict.get(key), params.get('num_evals'))
 
 
         for checkedout in checkedouts:
-            eval.return_item('rating', checkedout.get('rating_num'), checkedout.get('id'))
+            rating_num = checkedout.get('rating_num')
+            _id = checkedout.get('id')
+            eval.return_item('rating', rating_num, _id)
             with self.assertRaises(ValueError):
                 #when tried to return a second time, should raise an error
-                eval.return_item('rating', checkedout.get('rating_num'), checkedout.get('id'))
+                eval.return_item('rating', rating_num, _id)
+
 
         #after everything done, should be 1 item remaining in finished_pool
-        self.assertEquals(len(eval.tested_pool),1)
+        self.assertEquals(len(eval.tested_pool), 1)
         left_over = eval.tested_pool[0]
         self.assertEquals(left_over.id, 3)
         self.assertEquals(len(left_over.in_limbo), 0)
-        self.assertEquals(len(left_over.has_been_rated), Individual.default_num_evaluations)
+        self.assertEquals(len(left_over.has_been_rated), params.get('num_evals'))
 
 
 class TestEvalWithRandData(TestCase):
@@ -130,10 +154,10 @@ class TestEvalWithRandData(TestCase):
     def test1(self):
 
         items_to_evaluate = 50
-        feedback_after_x_complete_item_ratings = items_to_evaluate+1 #we dont want this to ever occur
+        minimum_finished = items_to_evaluate + 1  # we dont want this to ever occur
         num_eval = 5
 
-        eval = Evaluate_pool(None, feedback_after_x_complete_item_ratings, num_eval)
+        eval = Evaluate_pool(None, {'minimum_finished': minimum_finished, 'num_evals': num_eval})
 
         def test_data():
             mock = []
@@ -159,8 +183,6 @@ class TestEvalWithRandData(TestCase):
             checkedouts.append(checkedout)
             checkedout = eval.get_item('rater'+str(counter))
 
-
-
         self.assertEquals(len(checkedouts), items_to_evaluate * num_eval)
         print "-------------------------------"
         for i in range(0, len(items)):
@@ -174,6 +196,7 @@ class TestEvalWithRandData(TestCase):
 
         results = eval.tested_pool
 
+        #test to see that data tallies
         for i in range(0, len(items)):
 
             item__data = items[i].get('test_data')
